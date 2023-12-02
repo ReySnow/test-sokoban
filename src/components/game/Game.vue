ac7a8c5f548a4a13af81bbc27ea645d1b899469c
ReySnow <1286653145@qq.com>
2023-12-02 22:39:11 +0800

    feat: 实现放置点渲染

--- src/components/game/Game.vue
+++ src/components/game/Game.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
     <Map></Map>
+
+    <template v-for="target in targets">
+      <Target :x="target.x" :y="target.y"></Target>
+    </template>
     <Player></Player>
     <template v-for="cargo in cargos">
       <Cargo :x="cargo.x" :y="cargo.y"></Cargo>
@@ -10,10 +14,13 @@
 
 <script setup lang="ts">
 import { useCargoStore } from '@/store/cargo'
+import { useTargetStore } from '@/store/target'
 import Cargo from './Cargo.vue'
 import Map from './Map.vue'
 import Player from './Player.vue'
+import Target from './Target.vue'
 
+const { targets } = useTargetStore()
 const { cargos, addCarge, createCargo } = useCargoStore()
 
 addCarge(createCargo({ x: 2, y: 2 }))
---
+++ src/components/game/Target.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="absolute" :style="position">
+    <img :src="targetImg" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import targetImg from '@/assets/target.png'
+import { usePositon } from '@/composables/usePosition'
+
+interface Props {
+  x: number
+  y: number
+}
+const props = defineProps<Props>()
+const { position } = usePositon(props)
+</script>
+
+<style scoped></style>
